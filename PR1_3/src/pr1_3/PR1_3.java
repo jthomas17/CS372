40ac4f6a17464ad1c1551b1ccd112a70dd1310e0
jthomas17 <jthomas17@my.whitowrth.edu>
2015-01-06 15:12:37 -0800

    PR1_3

---
+++ PR1_3/src/pr1_3/PR1_3.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pr1_3;
+
+/**
+ *
+ * @author jthomas17
+ */
+public class PR1_3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /**
+        * @param initialize string name to a name
+        */
+         String name = "Jack";
+         /**
+          * @param if new argument is entered give different name
+          */
+        if(args.length > 0 && args[0].equalsIgnoreCase("new name"))
+            name = "Pear";
+        /**
+          * @param if new argument is entered give different name
+          */
+        else if(args.length > 0 && args[0].equalsIgnoreCase("dif name"))
+            name = "bob";
+        /**
+          * @param if new argument is entered give different name
+          */
+        else if(args.length > 0 && args[0].equalsIgnoreCase("not this name"))
+            name = "Jain";
+        /**
+         * @param Prints out name based on arguments given
+         */
+        System.out.println(name);
+    }
+    
+}
