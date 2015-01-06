661c1380289a3aa29b1124ccbee4343b51a1a541
jthomas17 <jthomas17@my.whitowrth.edu>
2015-01-06 15:33:28 -0800

    PR1_4

--- PR1_3/src/pr1_3/PR1_3.java
+++ PR1_3/src/pr1_3/PR1_3.java
@@ -14,29 +14,30 @@ public class PR1_3 {
 
     /**
      * @param args the command line arguments
+     * go
      */
     public static void main(String[] args) {
         /**
-        * @param initialize string name to a name
+        * initialize string name to a name<code>name</code>
         */
          String name = "Jack";
          /**
-          * @param if new argument is entered give different name
+          *if new argument is entered give different name
           */
         if(args.length > 0 && args[0].equalsIgnoreCase("new name"))
             name = "Pear";
         /**
-          * @param if new argument is entered give different name
+          *if new argument is entered give different name
           */
         else if(args.length > 0 && args[0].equalsIgnoreCase("dif name"))
             name = "bob";
         /**
-          * @param if new argument is entered give different name
+          *if new argument is entered give different name
           */
         else if(args.length > 0 && args[0].equalsIgnoreCase("not this name"))
             name = "Jain";
         /**
-         * @param Prints out name based on arguments given
+         *Prints out name based on arguments given
          */
         System.out.println(name);
     }
--- PR1_4/src/pr1_4/PR1_4.java
+++ PR1_4/src/pr1_4/PR1_4.java
@@ -17,11 +17,25 @@ public class PR1_4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        /**
+         * intialize input
+         */
         Scanner input = new Scanner(System.in);
+        /**
+         * prints out <code>What is the radius of the circle?</code>
+         */
         System.out.println("What is the radius of the circle?");
+        /**
+         * intializes radius based off user input 
+         */
         int radious = input.nextInt();
+        /**
+         * give PI a value
+         */
         final double PI = 3.14;
+        /**
+         * calculates area of circle based off user radius
+         */
         double area = PI * radious * radious;
         System.out.println(area);       
     }
