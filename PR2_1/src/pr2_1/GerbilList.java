240b5b36d32a1142e83b3963971c81db040e113f
jthomas17 <jthomas17@my.whitowrth.edu>
2015-01-08 15:58:11 -0800

    PR2_1

---
+++ PR2_1/src/pr2_1/GerbilList.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pr2_1;
+import java.util.*;
+/**
+ *
+ * @author jthomas17
+ */
+public class GerbilList {
+    private List list = new ArrayList();
+    public void add(Gerbil gerbil){
+        list.add(gerbil);
+    }
+    public Gerbil get(int index){
+        return (Gerbil)list.get(index);
+    }
+    public int size(){
+        return list.size();
+    }
+}
---
+++ PR2_1/src/pr2_1/PR2_1.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pr2_1;
+import java.util.*;
+/**
+ *
+ * @author jthomas17
+ */
+public class PR2_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        GerbilList gerbils = new GerbilList();
+        for (int i = 0; i < 5; i++){
+            gerbils.add(new Gerbil(i));
+        }
+        for(int i = 0; i < gerbils.size(); i++){
+            GerbilHop.Hop(gerbils.get(i));
+            System.out.println();
+        }
+    }
+    
+}
