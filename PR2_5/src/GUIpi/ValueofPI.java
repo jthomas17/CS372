050a136b6140fc9dcc65248d90bd34816ee42595
jthomas17 <jthomas17@my.whitowrth.edu>
2015-01-09 15:10:52 -0800

    PR2_5

---
+++ PR2_5/src/GUIpi/ValueofPI.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUIpi;
+
+/**
+ *
+ * @author jthomas17
+ */
+public class ValueofPI {
+double _userInput = 0;
+    public ValueofPI(double c) {
+        findPI(c);
+    }
+
+    public double findPI(double userInput) {
+        _userInput = userInput;
+        double sum = 0;
+        int n = 1;
+
+        for (int a = 1; a < _userInput; a += 2) {
+            sum += (double) (n * (4.0 / a));
+            n = (n == -1) ? 1 : -1;
+        }
+        return sum;
+
+    }
+}
