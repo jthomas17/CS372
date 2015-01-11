c1d3c549e20fcb5739a1a6b16260e6fdce634b6e
jthomas17 <jthomas17@my.whitowrth.edu>
2015-01-11 14:48:22 -0800

    PR2_6

---
+++ PR2_6/src/PR2_6/HomeworkTracker.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package PR2_6;
+import java.util.*;
+/**
+ *
+ * @author jthomas17
+ */
+public class HomeworkTracker {
+    double _testScore = 0;
+    double _min = 0;
+    double _max = 0;
+    double _average = 0;
+    ArrayList<Double> grade = new ArrayList<Double>();
+    
+    HomeworkTracker(){
+    
+    }
+    public void add1(double c){
+        grade.add(c);
+    }
+    public double findMin(){
+        _min = grade.get(0);
+        return _min;
+    }
+    public double findMax(){
+        _max =  grade.get(grade.size()-1);
+        return _max;
+    }
+    public double average(){
+        double sum = 0;
+        for (int i = 0; i < grade.size(); i++){
+            sum+=grade.get(i);
+        }
+        return sum / grade.size();
+        }
+}
