770ae5ca5af6d8803833c9402757c3abebbcec27
jthomas17 <jthomas17@my.whitowrth.edu>
2015-01-15 15:30:50 -0800

    PR3_3

---
+++ PR3_3/src/PR3_3/Event.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package PR3_3;
+
+/**
+ *
+ * @author jthomas17
+ */
+public class Event {
+    
+}
