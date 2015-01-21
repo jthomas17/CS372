6354b70a96f187fec5aead7027f3bb5ceb18418c
jthomas17 <jthomas17@my.whitworth.edu>
2015-01-20 16:37:21 -0800

    PR4_2

---
+++ PR4_2/app/src/main/java/com/example/jarek/pr4_2/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.jarek.pr4_2;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    public void toggleLabel(View v){
+        TextView f = (TextView)this.findViewById(R.id.result);
+        Random rnd = new Random();
+        int x = rnd.nextInt(2);
+        if(x == 1){
+            f.setText("Heads");
+        }
+        else if (x == 0)
+            f.setText("Tails");
+    }
+}
