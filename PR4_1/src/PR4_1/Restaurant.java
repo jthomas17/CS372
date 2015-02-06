2448ea6c368251369e3eb5e7f9d7e1d4f679d6c4
Peter Tucker <ptucker@whitworth.edu>
2015-02-05 21:04:03 -0800

    Added feedback

--- PR4_1/src/PR4_1/Restaurant.java
+++ PR4_1/src/PR4_1/Restaurant.java
@@ -10,7 +10,7 @@ package PR4_1;
  * @author Jarek
  */
 public class Restaurant {
-    public String name;
+    public String name; //PT -- don't make these public. -10
     public String address;
     public double rating;
     public String notes;
